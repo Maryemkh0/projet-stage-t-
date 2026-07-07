6c3536637af00c907dbadeed170d1188f76abb95
Maryemkh0 <meriem.khemir@esprit.tn>
2026-07-07 11:20:14 +0100

    Initial commit: projet Spring Boot genere

---
+++ src/main/java/com/codix/cicdai/cicd_ai_app/CicdAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.codix.cicdai.cicd_ai_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAiAppApplication.class, args);
+	}
+
+}
